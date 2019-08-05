619f398aeec67cd3fb0e32c3be0d6486f004df9e
Justin Wang <junting.wang@thoughtworks.com>
2019-08-05 17:17:04 +0800

    feat: add user login logic and styles on mine page

--- src/pages/me/index.vue
+++ src/pages/me/index.vue
@@ -1,66 +1,101 @@
 <template>
   <div class="container">
-    <div class="user-info-wrapper">
-      <img class="user-info-avatar" :src="userInfo.avatarUrl" />
-      <p class="user-info-nickname">{{ userInfo.nickName }}</p>
+    <template v-if="!isLogin">
+      <button class='user-info-wrapper' open-type="getUserInfo" @getuserinfo='getUserInfo'>
+        <img class="user-info-avatar" :src="userDefaultImgPath" />
+        <p class="user-info-nickname">请点击登录</p>
+      </button>
+    </template>
+    <template v-else>
+      <div class="user-info-wrapper">
+        <img class="user-info-avatar" :src="userInfo.avatarUrl" />
+        <p class="user-info-nickname">{{ userInfo.nickName }}</p>
+      </div>
+    </template>
+    <div class="form-item">
+      <p>我的证书</p>
+      <img class="form-item-arrow-icon" :src="arrowImgPath"/>
     </div>
-    <div class="form-item">我的证书</div>
   </div>
 </template>
 
 <script>
+import userDefaultImgPath from '../../../static/images/user-default.png'
+import arrowImgPath from '../../../static/images/right-arrow.png'
 
 export default {
   data: {
-    userInfo: {}
+    userInfo: wx.getStorageSync('userInfo') || {},
+    userDefaultImgPath,
+    arrowImgPath
+  },
+  computed: {
+    isLogin: function () {
+      return Object.keys(this.userInfo).length > 0
+    }
   },
   methods: {
+    getUserInfo (e) {
+      if (!e.target.userInfo) return
 
+      this.userInfo = e.target.userInfo
+      wx.setStorageSync('userInfo', this.userInfo)
+    }
   },
-  onShow () {
-    wx.getUserInfo({
-      withCredentials: false,
-      success: res => {
-        this.userInfo = res.userInfo
-      },
-      fail: () => {},
-      complete: () => {}
-    })
+  onLoad () {
+    wx.setNavigationBarTitle({ title: '我的' })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.container {
-  width: 100%;
-  padding: 0 20px;
+  $avatar-size: 60px;
+  $form-height: 44px;
 
-  .user-info-wrapper{
+  .container {
     width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-    box-sizing: border-box;
+    padding: 0 23px;
 
-    .user-info-avatar {
-      width: 80px;
-      height: 80px;
-      border: 0;
-      border-radius: 50%;
+    .user-info-wrapper{
+      width: 100%;
+      height: 100px;
+      box-sizing: border-box;
+      font-size: 14px;
+
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      padding: 20px 23px;
+
+      .user-info-avatar {
+        width: $avatar-size;
+        height: $avatar-size;
+        border: 0;
+        border-radius: 50%;
+      }
+      .user-info-nickname {
+        margin-left: 16px;
+      }
     }
-    .user-info-nickname {
-      margin-top: 10px;
+
+    .form-item {
+      width: 100%;
+      height: $form-height;
+      box-sizing: border-box;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 14px;
+      margin-top: 24px;
+
+      &-arrow-icon {
+        position: relative;
+        width: $form-height;
+        height: $form-height;
+        right: -17px;
+      }
     }
   }
-
-  .form-item {
-    width: 100%;
-    padding: 10px;
-    border-style: solid;
-    border-color: black;
-    border-width: 0 0 1px 0;
-  }
-}
 </style>
 
