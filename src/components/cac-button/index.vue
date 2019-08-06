3097767c506e8f730362f41a9b5c5a59de5bcf25
Justin Wang <junting.wang@thoughtworks.com>
2019-08-06 15:37:34 +0800

    feat: add cac button component

---
+++ src/components/cac-button/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="cac-button" @click="click()">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    click: Function
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .cac-button {
+    width: 100%;
+    height: 44px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 14px;
+    border-radius: 6px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    background-color: white;
+    color: black;
+
+    &:active {
+      box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.4);
+    }
+  }
+</style>
+
+
