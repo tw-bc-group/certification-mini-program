5080ce9f9887e61a22c26dc6a52838f1fae67916
xudongdong <dongdong.xu1@thoughtworks.com>
2019-08-08 12:07:31 +0800

    style: remove global container padding sytle

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  padding: 200rpx 0;
+  /* padding: 200rpx 0; */
   box-sizing: border-box;
 }
 /* this rule will be remove */
