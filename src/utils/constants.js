1c656e8a8c47ff41fa51ed6e42d6fff5de5aceb5
xudongdong <dongdong.xu1@thoughtworks.com>
2019-08-20 17:41:24 +0800

    fix ci

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 let qrCodeReg = /http:\/\/cac.thoughtworks.cn:5000\/certification\//
 
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
   qrCodeReg = /https:\/\/cac.thoughtworks.cn\/certification\//
 }
 
